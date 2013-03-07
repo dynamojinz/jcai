285bb3a778966f972802d7e8f08dbf5cd1021075
jinz <jinzan5766@gmail.com>
2013-03-07 11:20:25 +0800

    initialize an empty project for jcai

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jcai::Application.config.secret_token = 'e32d9df073fdcedc85ec0a474ef3546b2e33c0bab7d2d1784c9931ea9db556f55bebdfcd8523f7683e7cf6e797bb34b8d079e8a7c836ffbc12c69bca90d503ed'
