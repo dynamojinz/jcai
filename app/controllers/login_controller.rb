6fb2d5fe2ce0e56ddacfcafdc8494ba25048b55d
dynamojinz <jinzan5766@gmail.com>
2013-03-18 23:06:08 +0800

    Add course and its relationship

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -21,7 +21,8 @@ class LoginController < ApplicationController
             when 's'
               redirect_to :controller => 'student/course', :action => 'index'
             when 'a'
-              redirect_to :controller => 'course_admin', :action => 'list'
+              #redirect_to :controller => 'course_admin', :action => 'list'
+              redirect_to @admin_course
           end
         else
           flash[:notice] = "用户名或密码输错了！"
